264372efb19bb3b067d967bdb17c24e65e51aeb3
Anton Blagoveshchenskiy <anton@defdone.com>
2023-10-05 18:34:44 +0300

    workable modal added

--- frontend/src/components/molecules/account-row-element.tsx
+++ frontend/src/components/molecules/account-row-element.tsx
@@ -1,37 +1,61 @@
-import { Box, Flex, HStack, Text, VStack } from "@chakra-ui/react";
+import { Box, Flex, HStack, Text, VStack, Button } from "@chakra-ui/react";
+
+import AccountModal from "./account-modal";
+import { useState } from "react";
 
 const AccountRowElement = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsOpen(false); // Закрываем модальное окно
+  };
   return (
-    <Flex
-      w="100%"
-      p="16px 32px"
-      justify="space-between"
-      borderBottom="1px solid"
-      borderBottomColor="red"
-    >
-      <VStack align="left">
-        <Text fontSize="16px">Address</Text>
-        <Text fontSize="18px">0xAf3A84BACF2B705ea05a1197Cf87EF679eB7Ed3F</Text>
-      </VStack>
-      <VStack align="left">
-        <Text fontSize="16px">Balance</Text>
-        <Text fontSize="18px">100.00</Text>
-      </VStack>
-      <HStack>
+    <>
+      <Flex
+        w="100%"
+        p="16px 32px"
+        justify="space-between"
+        borderBottom="1px solid"
+        borderBottomColor="red"
+      >
         <VStack align="left">
-          <Text fontSize="16px">TX Count</Text>
-          <Text fontSize="18px">0</Text>
-        </VStack>
-        <VStack align="left">
-          <Text fontSize="16px">Index</Text>
-          <Text fontSize="18px">5</Text>
+          <Text fontSize="16px">Address</Text>
+          <Text fontSize="18px">
+            0xAf3A84BACF2B705ea05a1197Cf87EF679eB7Ed3F
+          </Text>
         </VStack>
         <VStack align="left">
           <Text fontSize="16px">Balance</Text>
           <Text fontSize="18px">100.00</Text>
         </VStack>
-      </HStack>
-    </Flex>
+        <HStack>
+          <VStack align="left">
+            <Text fontSize="16px">TX Count</Text>
+            <Text fontSize="18px">0</Text>
+          </VStack>
+          <VStack align="left">
+            <Text fontSize="16px">Index</Text>
+            <Text fontSize="18px">5</Text>
+          </VStack>
+          <VStack align="left">
+            <Text fontSize="16px">Balance</Text>
+            <Text fontSize="18px">100.00</Text>
+          </VStack>
+        </HStack>
+        <HStack>
+          <VStack align="center" justify="center">
+            <Button fontSize="16px" onClick={handleOpenModal}>
+              Open modal
+            </Button>
+          </VStack>
+        </HStack>
+      </Flex>
+      <AccountModal isOpen={isOpen} onClose={handleCloseModal} />
+    </>
   );
 };
 

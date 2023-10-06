1920202d01fb3771ec020512a9e9d930601acde2
Anton Blagoveshchenskiy <anton@defdone.com>
2023-10-06 17:36:26 +0300

    mobile device detection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, HStack, Text } from "@chakra-ui/react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,18 +11,35 @@ import Navbar from "./components/organisms/navbar";
 import Accounts from "./components/pages/accounts";
 import Blocks from "./components/pages/blocks";
 
-export const App = () => (
-  <ChakraProvider theme={fondantTheme}>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Accounts />} />
-        <Route path="/blocks" element={<Blocks />} />
-        <Route path="/deploys" element={<div>Dploys</div>} />
-        <Route path="/events" element={<div>Events</div>} />
-        <Route path="/logs" element={<div>Logs</div>} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
-  </ChakraProvider>
-);
+export const App = () => {
+  const isMobile = () => {
+    const deviceinfo = navigator.userAgent;
+    const regexp = /android|iphone|kindle|ipad/i;
+    const isSmallScreen = window.innerWidth < 768;
+    return regexp.test(deviceinfo) || isSmallScreen;
+  };
+
+  if (isMobile()) {
+    return (
+      <HStack width="100%" justify="center" fontSize="28px" fontWeight="600">
+        <Text align="center">Used device and resolution is unsupported</Text>
+      </HStack>
+    );
+  }
+
+  return (
+    <ChakraProvider theme={fondantTheme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Accounts />} />
+          <Route path="/blocks" element={<Blocks />} />
+          <Route path="/deploys" element={<div>Dploys</div>} />
+          <Route path="/events" element={<div>Events</div>} />
+          <Route path="/logs" element={<div>Logs</div>} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  );
+};

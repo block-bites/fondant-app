e8c89a9fdbb538c829412ab1990a30ca0f6383a0
Anton Blagoveshchenskiy <anton@defdone.com>
2023-10-18 18:58:44 +0300

    server tab added

--- frontend/src/components/organisms/tab-server.tsx
+++ frontend/src/components/organisms/tab-server.tsx
@@ -1,8 +1,8 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Select, Input, Switch } from "@chakra-ui/react";
 
 export default function ServerTab() {
   return (
-    <Flex>
+    <Flex direction="column" maxW="658px">
       <Text
         fontFamily="secondary"
         fontSize="48px"
@@ -11,6 +11,91 @@ export default function ServerTab() {
       >
         Server
       </Text>
+      <Flex direction="column" gap="32px" mt="56px">
+        <Flex direction="column" gap="32px" pb="24px">
+          <Flex direction="column" gap="16px">
+            <Select>
+              <option value="option1">0.0.0.0</option>
+              <option value="option2">192.168.1.1</option>
+              <option value="option3">192.168.0.2</option>
+            </Select>
+            <Text fontSize="12px" color="grey.400" lineHeight="120%">
+              The server will accept RPC connections on the following host and
+              port.
+            </Text>
+          </Flex>
+          <Input
+            size="sm"
+            placeholder="0.0.0.0"
+            variant="filled"
+            h="32px"
+            p="0 12px"
+            _placeholder={{ color: "gray.400", fontSize: "14px" }}
+            borderRadius="4px"
+          />
+          <Input
+            size="sm"
+            placeholder="0.0.0.0"
+            variant="filled"
+            h="32px"
+            p="0 12px"
+            _placeholder={{ color: "gray.400", fontSize: "14px" }}
+            borderRadius="4px"
+          />
+        </Flex>
+        <Flex justifyContent="space-between" gap="32px">
+          <Flex direction="column" gap="8px">
+            <Text
+              fontFamily="secondary"
+              color="grey.400"
+              lineHeight="170%"
+              fontWeight="600"
+            >
+              Automine
+            </Text>
+            <Switch colorScheme="blue" size="lg" isChecked />
+          </Flex>
+          <Text fontSize="12px" color="grey.400" lineHeight="120%">
+            Process transactions instantaneously.
+          </Text>
+        </Flex>
+        <Flex pb="24px" justifyContent="space-between" gap="32px">
+          <Flex direction="column" gap="8px">
+            <Text
+              fontFamily="secondary"
+              color="grey.400"
+              lineHeight="170%"
+              fontWeight="600"
+            >
+              Error on transaction failure
+            </Text>
+            <Switch colorScheme="blue" size="lg" isChecked />
+          </Flex>
+          <Text fontSize="12px" color="grey.400" lineHeight="120%" maxW="210px">
+            failures wact only all detectable era the statubled, a in action
+            transaction receipt. Disabling this feature will make Ganache handle
+            transaction failures like other Ethereum clients.{" "}
+          </Text>
+        </Flex>
+        <Flex gap="24px" direction="column">
+          <Text
+            fontFamily="secondary"
+            fontSize="24px"
+            lineHeight="155%"
+            fontWeight="400"
+          >
+            Chain Forking
+          </Text>
+          <Text
+            fontFamily="secondary"
+            color="grey.400"
+            lineHeight="170%"
+            fontWeight="600"
+          >
+            Error on transaction failure
+          </Text>
+        </Flex>
+      </Flex>
     </Flex>
   );
 }
--- frontend/src/components/organisms/tab-workspace.tsx
+++ frontend/src/components/organisms/tab-workspace.tsx
@@ -2,7 +2,7 @@ import { Button, Flex, Input, Text, Textarea } from "@chakra-ui/react";
 
 export default function WorkspaceTab() {
   return (
-    <Flex direction="column">
+    <Flex direction="column" w="100%" maxW="654px">
       <Text
         fontFamily="secondary"
         fontSize="48px"
@@ -16,7 +16,6 @@ export default function WorkspaceTab() {
         placeholder="Workspace name"
         mt="56px"
         variant="filled"
-        w="654px"
         h="32px"
         p="0 12px"
         _placeholder={{ color: "gray.400", fontSize: "14px" }}
@@ -28,20 +27,13 @@ export default function WorkspaceTab() {
       <Textarea
         borderRadius="4px"
         variant="filled"
-        w="654px"
         h="154px"
         placeholder="Fondant Project"
         p="8px 12px"
         mt="32px"
         _placeholder={{ color: "gray.400", fontSize: "14px" }}
       />
-      <Text
-        fontSize="12px"
-        color="grey.400"
-        mt="16px"
-        lineHeight="120%"
-        w="654px"
-      >
+      <Text fontSize="12px" color="grey.400" mt="16px" lineHeight="120%">
         Link Truffle projects to this workspace by adding their
         truffle-config.js or truffle.js file to this workspace. This will show
         useful contract and event data to better understand what's going on

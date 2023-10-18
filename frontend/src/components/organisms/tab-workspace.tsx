eaa7d12af42d4de3154aa9263a25653329d56e7d
Anton Blagoveshchenskiy <anton@defdone.com>
2023-10-18 16:35:36 +0300

    workspace component added

--- frontend/src/components/organisms/tab-workspace.tsx
+++ frontend/src/components/organisms/tab-workspace.tsx
@@ -1,8 +1,8 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Input, Text, Textarea } from "@chakra-ui/react";
 
 export default function WorkspaceTab() {
   return (
-    <Flex>
+    <Flex direction="column">
       <Text
         fontFamily="secondary"
         fontSize="48px"
@@ -11,6 +11,44 @@ export default function WorkspaceTab() {
       >
         Workspace
       </Text>
+      <Input
+        size="sm"
+        placeholder="Workspace name"
+        mt="56px"
+        variant="filled"
+        w="654px"
+        h="32px"
+        p="0 12px"
+        _placeholder={{ color: "gray.400", fontSize: "14px" }}
+      />
+      <Text fontSize="12px" color="grey.400" mt="16px" lineHeight="120%">
+        A friendly name for this workspace.
+      </Text>
+      <Textarea
+        variant="filled"
+        w="654px"
+        h="154px"
+        placeholder="Fondant Project"
+        p="8px 12px"
+        mt="32px"
+        _placeholder={{ color: "gray.400", fontSize: "14px" }}
+      />
+      <Text
+        fontSize="12px"
+        color="grey.400"
+        mt="16px"
+        lineHeight="120%"
+        w="654px"
+      >
+        Link Truffle projects to this workspace by adding their
+        truffle-config.js or truffle.js file to this workspace. This will show
+        useful contract and event data to better understand what's going on
+        under the hood.
+      </Text>
+      <Flex mt="56px" gap="24px">
+        <Button colorScheme="red"> Add Project</Button>
+        <Button color="grey.400">Remove Project</Button>
+      </Flex>
     </Flex>
   );
 }

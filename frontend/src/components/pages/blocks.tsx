550eebb7da0c9b8f23f99964bbf4fd68ee272c87
Karol Hetman <karol.hetman@protonmail.com>
2023-12-16 01:46:54 +0100

    Fixed errors with fetching blocks

--- frontend/src/components/pages/blocks.tsx
+++ frontend/src/components/pages/blocks.tsx
@@ -12,35 +12,48 @@ export default function Blocks() {
   const [blocks, setBlocks] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isLastPage, setIsLastPage] = useState(false);
 
   const DISPLAY_PER_PAGE = 10;
 
   useEffect(() => {
-    const client = new CasperServiceByJsonRPC(
-      "http://localhost:3001/net/1/rpc"
-    );
+    const client = new CasperServiceByJsonRPC("http://localhost:3001/net/1/rpc");
 
     const fetchBlocks = async () => {
+      setLoading(true);
+      setError(null);
       try {
         const latestBlockInfo = await client.getLatestBlockInfo();
         if (latestBlockInfo && latestBlockInfo.block) {
           const currentHeight = latestBlockInfo.block.header.height;
-          const blockHeights = Array.from(
-            { length: DISPLAY_PER_PAGE },
-            (_, i) => currentHeight - i - currentPage * DISPLAY_PER_PAGE
-          );
+
+          const blockHeights = [];
+          for (let i = 0; i < DISPLAY_PER_PAGE; i++) {
+            const height = currentHeight - i - (currentPage - 1) * DISPLAY_PER_PAGE;
+            if (height >= 0) {
+              blockHeights.push(height);
+            }
+          }
+
+          if (blockHeights.length === 0) {
+            setBlocks([]);
+            setIsLastPage(true);
+            return;
+          } else {
+            setIsLastPage(false);
+          }
 
           const blockInfoPromises = blockHeights.map((height) =>
             client.getBlockInfoByHeight(height)
           );
-          const blockInfos: GetBlockResult[] = await Promise.all(
-            blockInfoPromises
-          );
+          const blockInfos: GetBlockResult[] = await Promise.all(blockInfoPromises);
           const newBlocks = blockInfos
             .map((blockInfo) => blockInfo.block)
             .filter((block): block is JsonBlock => block !== null);
+
           setBlocks(newBlocks);
+          setIsLastPage(newBlocks.length < DISPLAY_PER_PAGE);
         }
       } catch (err) {
         setError(err instanceof Error ? err.message : String(err));
@@ -53,11 +66,13 @@ export default function Blocks() {
   }, [currentPage]);
 
   const handlePrevious = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
   };
 
   const handleNext = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
+    if (!isLastPage) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
   };
 
   if (loading) {
@@ -66,6 +81,10 @@ export default function Blocks() {
 
   if (error) {
     return <Text>Error fetching blocks: {error}</Text>;
+  }
+
+  if (blocks.length === 0 && !loading) {
+    return <Text>No blocks available to display.</Text>;
   }
 
   return (
@@ -77,7 +96,7 @@ export default function Blocks() {
         <VStack w="100%" maxW="1440px" gap="0">
           {blocks.map((block) => (
             <BlockRowElement
-              key={block.hash} // Add a key for React list rendering
+              key={block.hash}
               height={block.header.height}
               era={block.header.era_id}
               deploys={block.body.deploy_hashes.length}
@@ -88,10 +107,12 @@ export default function Blocks() {
         </VStack>
       </Flex>
       <Flex justify="center" mt="4">
-        <Button onClick={handlePrevious} isDisabled={currentPage === 0}>
+        <Button onClick={handlePrevious} isDisabled={currentPage === 1}>
           Previous
         </Button>
-        <Button onClick={handleNext}>Next</Button>
+        <Button onClick={handleNext} isDisabled={isLastPage}>
+          Next
+        </Button>
       </Flex>
     </>
   );

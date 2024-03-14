bb747eb00916ad46b61f728d336c2915483a4285
commondrum <karol.hetman@protonmail.com>
2024-03-14 01:28:38 +0000

    command runner improvment

--- cctl-container/api/src/utils/command_runner.rs
+++ cctl-container/api/src/utils/command_runner.rs
@@ -1,17 +1,23 @@
-use serde_derive::Deserialize;
+use serde_derive::Serialize;
+use serde_json;
 use std::collections::HashMap;
 use std::fs;
-use std::process::Command;
+use std::process::{Command, Output};
+use std::str;
 
-#[derive(Deserialize)]
-struct Commands {
-    commands: HashMap<String, String>,
+#[derive(Serialize)]
+struct CommandResult {
+    status: String,
+    stdout: String,
+    stderr: String,
 }
 
-fn run_command(command_input: &str) {
-    let json_file_path = "/commands.json"
+
+fn run_command(command_input: &str) -> String {
+    let json_file_path = "commands.json";
     let file_contents = fs::read_to_string(json_file_path).expect("Failed to read JSON file");
-    let commands: Commands = serde_json::from_str(&file_contents).expect("Failed to parse JSON");
+    let commands: HashMap<String, String> =
+        serde_json::from_str(&file_contents).expect("Failed to parse JSON");
 
     let mut parts = command_input.split_whitespace();
     let command = parts.next().expect("No command provided");
@@ -19,13 +25,7 @@ fn run_command(command_input: &str) {
 
     let replaced_args: Vec<String> = args
         .iter()
-        .map(|&arg| {
-            commands
-                .commands
-                .get(arg)
-                .cloned()
-                .unwrap_or_else(|| arg.to_string())
-        })
+        .map(|&arg| commands.get(arg).cloned().unwrap_or_else(|| arg.to_string()))
         .collect();
 
     let output = Command::new(command)
@@ -33,7 +33,18 @@ fn run_command(command_input: &str) {
         .output()
         .expect("Failed to execute command");
 
-    println!("Status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    let command_result = process_output(output);
+
+    serde_json::to_string(&command_result).expect("Failed to serialize command result")
+}
+
+fn process_output(output: Output) -> CommandResult {
+    CommandResult {
+        status: match output.status.success() {
+            true => "success".to_string(),
+            false => format!("error {}", output.status),
+        },
+        stdout: String::from_utf8_lossy(&output.stdout).to_string(),
+        stderr: String::from_utf8_lossy(&output.stderr).to_string(),
+    }
 }

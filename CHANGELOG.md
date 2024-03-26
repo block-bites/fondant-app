e405775df2ab088f7ea3005d54e1f45bf52055ab
Karol Hetman <karol.hetman@protonmail.com>
2024-03-26 15:49:04 +0100

    Changelog updated and sanitization for args added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,19 @@ All notable changes to this project will be documented in this file.
  
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [1.0.5] - 2024-03-26
+
+### Added
+
+#### Frontend
+- No changes.
+
+#### Backend
+- Exchanged NCTL container to in-house created CCTL container
+- Discarded Lean API. Now the API uses rust rocket framework.
+- Merged API and CCTL containers.
+- Run endpoint that is compatible with all CCTL commands.
  
 
 ## [1.0.0] - 2024-01-15
--- cctl-container/api/src/utils/mod.rs
+++ cctl-container/api/src/utils/mod.rs
@@ -16,23 +16,44 @@ pub struct CommandResult {
 
 pub fn run_command(command: &str, args: Option<Vec<String>>) -> Result<CommandResult, String> {
     let json_file_path = "commands.json";
+
     let file_contents = fs::read_to_string(json_file_path)
         .map_err(|e| format!("Failed to read JSON file: {}", e))?;
-    
+
     let command_map: HashMap<String, String> = serde_json::from_str(&file_contents)
         .map_err(|e| format!("Failed to parse JSON: {}", e))?;
 
     let final_command = command_map.get(command)
         .ok_or_else(|| format!("Command not found: {}", command))?;
 
+    let sanitized_args = sanitize_args(args)?;
+
     let output = Command::new("bash")
         .arg("-c")
         .arg(&final_command)
-        .args(args.unwrap_or_default())
+        .args(sanitized_args.unwrap_or_default())
         .output()
         .map_err(|e| format!("Failed to execute command: {}", e))?;
 
     Ok(process_output(output))
+}
+
+fn sanitize_args(args: Option<Vec<String>>) -> Result<Option<Vec<String>>, String> {
+    if let Some(arg_list) = args {
+        let sanitized_args: Vec<String> = arg_list
+            .into_iter()
+            .map(|arg| {
+                if arg.contains(";") || arg.contains("&&") || arg.contains("||") {
+                    Err(format!("Invalid argument: {}", arg))
+                } else {
+                    Ok(arg)
+                }
+            })
+            .collect::<Result<_, _>>()?;
+        Ok(Some(sanitized_args))
+    } else {
+        Ok(None)
+    }
 }
 
 pub fn process_output(output: Output) -> CommandResult {

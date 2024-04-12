3af0b10a77044383d896637717df2d7651c816d6
Karol Hetman <karol.hetman@protonmail.com>
2024-04-12 11:55:14 +0200

    Docker development file

--- setup/rebuild.sh
+++ setup/rebuild.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd ..
+
+if [[ "${PWD##*/}" != "fondant-app" ]]; then
+    echo "Please execute this script in the fondant-app folder."
+    exit 1
+fi
+
+
 docker-compose down
-docker-compose up --build
+
+docker-compose build
+
+docker-compose up

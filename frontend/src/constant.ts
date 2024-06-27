ad7897ae4e440422af8ca395082b58920b07b256
wojtek-blockbites <wojtek@blockbit.es>
2024-06-27 15:27:15 +0200

    env var renamed

--- frontend/src/constant.ts
+++ frontend/src/constant.ts
@@ -1,4 +1,4 @@
 export const NUM_OF_NODES_CONSIDERED_RUNNING = 5
 export const DEFAULT_NODE_NUMBER = 1
-export const NODE_URL = `http://localhost:${process.env.REACT_APP_PROXY_PORT}`
+export const NODE_URL = `http://localhost:${process.env.REACT_APP_PORT}`
 export const NODE_URL_PORT = "http://localhost:3001"
